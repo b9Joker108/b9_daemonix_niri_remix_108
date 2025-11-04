406ebea45c2ba4b2706cf3ad499c72ebff1a26b7
DXC-0 <inscription-site-1@protonmail.com>
2025-11-05 00:14:42 +0100

    Update custom.sh

--- build_files/custom.sh
+++ build_files/custom.sh
@@ -5,6 +5,6 @@ sed -i 's/^NAME=.*/NAME="Fedora (Daemonix)"/' /etc/os-release
 sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Fedora (Daemonix)"/' /etc/os-release
 
 # Icon theme
-git clone git@github.com:DXC-0/Flatery.git
+git clone https://github.com/DXC-0/Flatery.git
 cd Flatery
 bash install.sh
